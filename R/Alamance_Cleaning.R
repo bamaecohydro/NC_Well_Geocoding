33182670b9f77a0ef3967af99b07d3158c19918a
FloodHydrology <cnjones7@ua.edu>
2025-02-11 19:26:51 -0600

    cleaning and scraping initial commit

---
+++ R/Alamance_Cleaning.R
@@ -0,0 +1,108 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Title: NC Well Data Cleaning for Geocoding (Alamanace County)
+# Coder: C. Nathan Jones (natejones@ua.edu)
+# Date: 2/11/2025
+# Subject: Prep scraped data for GeoCoding in ArcGIS
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 1.0 Setup workspace ----------------------------------------------------------
+
+#Clear memory
+remove(list = ls())
+
+# install.packages("remotes")
+#remotes::install_github("slu-openGIS/postmastr")
+
+#Load packages
+library(tidyverse)
+library(lubridate)
+library(tidytext)
+library(postmastr)
+library(tidycensus)
+
+
+#download and tidy data
+df <- read_csv('temp/alamance_scraping.csv') 
+
+# 2.0 Parse data ---------------------------------------------------------------
+# Function to remove the name (if exists) from the string
+remove_name <- function(address) {
+  #If NA
+  if (is.na(address)) {
+    return(NA_character_)  # Return NA if the input is NA
+  }
+  
+  #Break address into parts
+  parts <- str_split(address, ";")[[1]]
+  
+  # Check if the first component starts with a number (indicating it's likely an address)
+  if (!str_detect(parts[1], "^[0-9]")) {
+    # If it doesn't start with a number, assume it's a name and remove it
+    parts <- parts[-1]  # Remove the first part (the name)
+  }
+  
+  # Reconstruct the string without the name
+  return(paste(parts, collapse = ";"))
+}
+
+# Apply the function to remove names
+df<-df %>%
+  mutate(scraped_info = address) %>% 
+  mutate(address = map_chr(address, remove_name))
+
+#Create address list
+df <- df %>% pm_identify(var='address') 
+
+#Parse state and zip
+output <- df %>% 
+  pm_prep(var = 'address', type='street') %>% 
+  pm_postal_parse() %>% 
+  pm_state_parse() 
+
+#Create City List
+city_list <- pm_dictionary(type = "city", filter = "NC", locale = "us")
+
+#Identify records without city
+pm_city_none(output, dictionary = city_list) %>% View()
+
+#Add Elon College and Snow camp to dictionary
+missing_city <- pm_append(
+  type = "city", 
+  input = c('Elon College', 'Snow Camp'), 
+  output = c('Elon College', 'Snow Camp'), locale= "us")
+
+#update City Dictionary
+city_list <- pm_dictionary(
+  type="city", 
+  filter = "NC", 
+  append = missing_city, 
+  locale = "us")
+
+#Parse City
+output <- output %>% pm_city_parse(dictionary = city_list)
+
+#Left Join and tidy
+df <- left_join(df, output) 
+
+#DEfine pre-geocode error col
+df <- df %>%
+  mutate(scraping_error = if_any(c(pm.address, pm.city, pm.state, pm.zip), is.na) %>% as.integer())
+
+#Tidy
+df<-df %>% 
+  mutate(state = "NC") %>% 
+  mutate(county = "Alamance") %>% 
+  select(
+    sample_id, 
+    scraped_info,
+    address_full = address, 
+    street_address = pm.address, 
+    city = pm.city, 
+    county,
+    state, 
+    zip = pm.zip,
+    scraping_error)
+
+#Export
+write_csv(df, "temp//alamance_cleaning.csv")
+   
---
+++ R/Alamance_Scraping.R
@@ -0,0 +1,128 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Title: NC Well Data Scraping
+# Coder: C. Nathan Jones (natejones@ua.edu)
+# Date: 2/11/2025
+# Subject: Scrape data from NC Well database website
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 1.0 Setup workspace ----------------------------------------------------------
+#Clear memory
+remove(list=ls())
+
+#load libraries of interest
+library(tidyverse)
+library(stringr)
+library(rvest)
+library(httr)
+library(pdftools)
+library(parallel)
+
+#Define URL to scrape
+url <- "https://celr.dph.ncdhhs.gov/microBiology?client.rasclientId=566000271EH&filterBy=1&recentDay=5&docFrom=&docTo="
+
+# 3.0 Create list of pdf's to scrape -------------------------------------------
+# Read HTML content
+df <- read_html(url) %>% html_elements("a") %>% html_attr("href") %>% as_tibble()
+
+#Select html dataset
+df <- df %>% filter(str_detect(value, "showReportPublic"))
+
+#Identify uid and website
+df <- df %>% 
+  #Sample ID based on strng
+  mutate(sample_id = str_extract(value, "(?<=No=)[^&]+")) %>% 
+  #Website
+  mutate(report_url = paste0('https://celr.dph.ncdhhs.gov/',value)) %>% 
+  #remove value col
+  select(-value)
+
+# 3.0 Create function to scrape addresses --------------------------------------
+#Create function to pull address from pdf
+address_fun<- function(n){
+  
+  #load libraries of interest
+  library(tidyverse)
+  library(stringr)
+  library(rvest)
+  library(httr)
+  library(pdftools)
+  
+  #define sample_id
+  sample_id <- df$sample_id[n]
+  
+  # Define the URL of the PDF
+  pdf_url <- df$report_url[n] 
+  
+  # Download the PDF
+  pdf_file <- paste0("temp//",sample_id,".pdf")
+  response <- GET(pdf_url, write_disk(pdf_file, overwrite = TRUE))
+  pdf_data <- pdf_data(pdf_file)[[1]] %>% as_tibble()
+  
+  #Define location in pdf for extraction
+  x_min <- pdf_data %>% filter(text == "Name") %>% select(x) %>% pull()
+  y_min <- pdf_data %>% filter(text == "Name") %>% select(y) %>% pull()
+  y_max <- pdf_data %>% filter(text == "EIN:") %>% select(y) %>% pull()
+  
+  #redefine y_max for older samples without EIN designation
+  if(is_empty(y_max)){
+    y_max <- pdf_data %>% filter(text == "StarLiMS") %>% select(y) %>% pull()
+  }
+  
+  #filter to location on pdf
+  pdf_data <- pdf_data %>% filter(x>=x_min) %>% filter(y>y_min) %>% filter(y<y_max)
+  
+  #Reconstruct text
+  y_tol <- 2
+  address<-pdf_data %>% 
+    mutate(y_group = round(y/y_tol)*y_tol) %>% 
+    group_by(y_group) %>% 
+    arrange(y_group, x) %>% 
+    summarise(line_text = paste(text, collapse = " ")) %>% 
+    pull(line_text) %>% 
+    paste(collapse = ";")
+  
+  #delete pdf file
+  file.remove(pdf_file)
+  
+  #Output
+  tibble(sample_id, address)
+  }
+
+#4.0 Run address scraping function ---------------------------------------------
+#Create an error handling wrapper function
+error_fun<-function(n){
+  tryCatch(
+    expr = address_fun(n), 
+    error = function(e)
+      tibble(
+        sample_id=df$sample_id[n],
+        address= NA)
+  )
+}  
+
+#setup paralell processing environment
+n.cores<- detectCores() - 1
+
+#Create clusters
+cl<-makeCluster(n.cores)
+
+#Export data to cluter environments
+clusterExport(cl, c("address_fun", "df"))
+
+#Now run function
+t0<-Sys.time()
+output<-parLapply(
+  cl=cl,
+  seq(1, nrow(df)),#
+  error_fun)
+tf<-Sys.time()
+tf-t0
+
+#Now, bind rows from list output
+output<-output %>% bind_rows()
+
+#Stop the clusters
+stopCluster(cl)
+
+#export
+write_csv(output, "temp//alamance_scraping.csv")
